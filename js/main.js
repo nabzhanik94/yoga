f85d2a0a1ad408adb48222df681d1d6432344978
naberezhnykh_zhan_job <nabzhanik94@gmail.com>
2020-03-14 23:19:48 +0300

    Добавил слайдер, пока что не разобрался, как сделать такой же слайдер, как и в макете

--- js/main.js
+++ js/main.js
@@ -1,25 +1,30 @@
-$(document).ready(function(){
-    $('.slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        prevArrow: $('.arrows__left'),
-        nextArrow: $('.arrows__right'),
-        responsive: [
-        {
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 770,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
+// $(document).ready(function(){
+    // $('.slider').slick();
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         prevArrow: $('.arrows__left'),
+//         nextArrow: $('.arrows__right'),
+//         responsive: [
+//         {
+//             breakpoint: 1200,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 1
+//             }
+//         },
+//         {
+//             breakpoint: 770,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1
+//             }
+//         }
         
-        ]
-    });
-})
+//         ]
+//     });
+// })
+// $(document).ready(function(){
+//     $('.slider').slick({
+//       setting-name: setting-value
+//     });
+//   });
